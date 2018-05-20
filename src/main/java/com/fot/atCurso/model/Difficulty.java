093b872e3d9aeb28b5f36d2fdffe68eead1d56f6
Orrequia <orrequia8@gmail.com>
2018-05-20 13:10:04 +0200

    Copiado de cuestionarioatSistemas

---
+++ src/main/java/com/fot/atCurso/model/Difficulty.java
@@ -0,0 +1,27 @@
+package com.fot.atCurso.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Difficulty {
+	
+	@Id
+	@GeneratedValue
+	private Integer idDifficulty;
+	
+	@Column(nullable = false, unique = true)
+	private String name;
+	
+	/*@OneToMany(fetch = FetchType.LAZY, mappedBy = Question.FIELD_DIFFICULTY)
+	private List<Difficulty> difficulty;*/
+}
