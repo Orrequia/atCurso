80de4c291db03f86e1c68514b5ebb44680885545
Orrequia <orrequia8@gmail.com>
2018-05-20 13:10:07 +0200

    Copiado de cuestionarioatSistemas

---
+++ src/main/java/com/fot/atCurso/model/Question.java
@@ -0,0 +1,54 @@
+package com.fot.atCurso.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Question {
+
+	public static final String FIELD_IDTAG = "idTag";
+	public static final String FIELD_IDDIFFICULTY = "idDifficulty";
+	public static final String FIELD_IDQUESTION = "idQuestion";
+	
+	@Id
+	@GeneratedValue
+	@Column(name=FIELD_IDQUESTION)
+	private Integer idQuestion;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@JoinColumn(name = FIELD_IDTAG)
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Tag tag;
+	
+	@JoinColumn(name = FIELD_IDDIFFICULTY)
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Difficulty dificulty;
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name=FIELD_IDQUESTION, referencedColumnName=FIELD_IDQUESTION)
+	private List<Answer> answer;
+	
+	/*@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(
+      name="questionary_question",
+      joinColumns=@JoinColumn(name=FIELD_IDQUESTION),
+      inverseJoinColumns=@JoinColumn(name=Questionary.FIELD_IDQUESTIONARY))
+	private List<Questionary> questionary;*/
+}
