c9b1d4a9c123c0cc23a363de5bb524f666bef367
Orrequia <orrequia8@gmail.com>
2018-05-20 12:17:05 +0200

    Estructura

---
+++ src/main/java/com/fot/atCurso/AtCursoApplication.java
@@ -0,0 +1,12 @@
+package com.fot.atCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtCursoApplication.class, args);
+	}
+}
