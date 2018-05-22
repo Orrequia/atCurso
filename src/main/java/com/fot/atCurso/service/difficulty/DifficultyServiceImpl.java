735b68ec31714c0e729e615eb4efeb582dc7c019
Orrequia <orrequia8@gmail.com>
2018-05-22 09:56:38 +0200

    Implementando métodos de la interfaz abstracta

--- src/main/java/com/fot/atCurso/service/difficulty/DifficultyServiceImpl.java
+++ src/main/java/com/fot/atCurso/service/difficulty/DifficultyServiceImpl.java
@@ -1,13 +1,45 @@
 package com.fot.atCurso.service.difficulty;
 
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.fot.atCurso.dao.DifficultyDAO;
+import com.fot.atCurso.model.Difficulty;
 
 @Service
 public class DifficultyServiceImpl implements DifficultyService {
 
 	@Autowired
 	DifficultyDAO difficultyDAO;
+	
+	@Override
+	public Difficulty create(Difficulty t) {
+		return difficultyDAO.save(t);
+	}
+
+	@Override
+	public void update(Difficulty t) {
+		difficultyDAO.save(t);
+	}
+
+	@Override
+	public Optional<Difficulty> findById(Integer id) {
+		return difficultyDAO.findById(id);
+	}
+
+	@Override
+	public Set<Difficulty> findAll(Pageable p) {
+		return difficultyDAO.findAll(PageRequest.of(p.getPageNumber(), p.getPageSize())).stream().collect(Collectors.toSet());
+	}
+
+	@Override
+	public void delete(Difficulty t) {
+		difficultyDAO.delete(t);
+	}
 }
