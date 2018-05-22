0ec4f9bf47d8bae676c959a4077dbadc401bba77
Orrequia <orrequia8@gmail.com>
2018-05-22 09:54:21 +0200

    extendiendo de AbstractService

--- src/main/java/com/fot/atCurso/service/question/QuestionService.java
+++ src/main/java/com/fot/atCurso/service/question/QuestionService.java
@@ -1,5 +1,8 @@
 package com.fot.atCurso.service.question;
 
-public interface QuestionService {
+import com.fot.atCurso.model.Question;
+import com.fot.atCurso.service.AbstractService;
+
+public interface QuestionService  extends AbstractService<Question, Integer> {
 
 }
