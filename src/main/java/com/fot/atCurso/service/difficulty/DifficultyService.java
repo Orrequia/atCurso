23a5050eba05084558bd75b5ec31415bd980f1a9
Orrequia <orrequia8@gmail.com>
2018-05-22 09:52:26 +0200

    extendiendo de AbstractService

--- src/main/java/com/fot/atCurso/service/difficulty/DifficultyService.java
+++ src/main/java/com/fot/atCurso/service/difficulty/DifficultyService.java
@@ -1,5 +1,8 @@
 package com.fot.atCurso.service.difficulty;
 
-public interface DifficultyService {
+import com.fot.atCurso.model.Difficulty;
+import com.fot.atCurso.service.AbstractService;
+
+public interface DifficultyService  extends AbstractService<Difficulty, Integer> {
 
 }
