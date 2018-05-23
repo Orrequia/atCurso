687610822a456a0ca92ee0208ceed0e45bc14e28
Orrequia <orrequia8@gmail.com>
2018-05-23 11:20:01 +0200

    Eliminado espacio de mas

--- src/main/java/com/fot/atCurso/service/tag/TagService.java
+++ src/main/java/com/fot/atCurso/service/tag/TagService.java
@@ -3,6 +3,6 @@ package com.fot.atCurso.service.tag;
 import com.fot.atCurso.model.Tag;
 import com.fot.atCurso.service.AbstractService;
 
-public interface TagService  extends AbstractService<Tag, Integer> {
+public interface TagService extends AbstractService<Tag, Integer> {
 
 }
