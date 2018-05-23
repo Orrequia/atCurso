e00d8e9d1f56bb5c82bf5d1ef2209ddd69fdd543
Orrequia <orrequia8@gmail.com>
2018-05-23 11:02:13 +0200

    Mapper Abstracto

---
+++ src/main/java/com/fot/atCurso/component/mapper/Mapper.java
@@ -0,0 +1,18 @@
+package com.fot.atCurso.component.mapper;
+
+import java.util.Set;
+
+public interface Mapper<M, D> {
+	
+	M dtoToModel(D dto);
+	
+	D modelToDto(M model);
+	
+	Set<M> dtoToModel(Set<D> dtos);
+	
+	Set<D> modelToDto(Set<M> models);
+	
+	Class<? extends D> dtoClazz();
+	
+	Class<? extends M> modelClazz();
+}
