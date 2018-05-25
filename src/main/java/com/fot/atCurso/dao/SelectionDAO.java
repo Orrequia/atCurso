688173786b138f0ac190380e5971b8388d53f1dd
Orrequia <orrequia8@gmail.com>
2018-05-25 12:56:08 +0200

    Creado DAO de Selection

---
+++ src/main/java/com/fot/atCurso/dao/SelectionDAO.java
@@ -0,0 +1,10 @@
+package com.fot.atCurso.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.fot.atCurso.model.Selection;
+
+@Repository
+public interface SelectionDAO extends GenericDAO<Selection>{
+
+}
