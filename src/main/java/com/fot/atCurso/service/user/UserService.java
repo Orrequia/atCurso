38e7fe700f805f9f32ea0fa1436988f3c544922c
Orrequia <orrequia8@gmail.com>
2018-05-25 13:02:31 +0200

    Añadido metodo para buscar por Email

--- src/main/java/com/fot/atCurso/service/user/UserService.java
+++ src/main/java/com/fot/atCurso/service/user/UserService.java
@@ -1,8 +1,16 @@
 package com.fot.atCurso.service.user;
 
+import java.util.Optional;
+
 import com.fot.atCurso.model.User;
 import com.fot.atCurso.service.AbstractService;
 
 public interface UserService extends AbstractService<User, Integer> {
+	
+	boolean isEqual(User u1, User u2);
+	
+	void setValues(User to, User from);
+	
+	Optional<User> findByEmail(String email);
 	
 }
