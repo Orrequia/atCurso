7fb8eeb555569407a17d56733d43edfc276e177c
Orrequia <orrequia8@gmail.com>
2018-05-25 12:56:25 +0200

    Creada interfaz de Selection Service

---
+++ src/main/java/com/fot/atCurso/service/selection/SelectionService.java
@@ -0,0 +1,8 @@
+package com.fot.atCurso.service.selection;
+
+import com.fot.atCurso.model.Selection;
+import com.fot.atCurso.service.AbstractService;
+
+public interface SelectionService extends AbstractService<Selection, Integer> {
+
+}
