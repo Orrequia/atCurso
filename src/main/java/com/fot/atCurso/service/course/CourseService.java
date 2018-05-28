ca6f1956011a7219c03128fa2e71e54e49a158d5
Orrequia <orrequia8@gmail.com>
2018-05-28 13:04:05 +0200

    Modificado nombres de los metodos

--- src/main/java/com/fot/atCurso/service/course/CourseService.java
+++ src/main/java/com/fot/atCurso/service/course/CourseService.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import com.fot.atCurso.exceptions.NotFoundException;
 import com.fot.atCurso.model.Course;
 import com.fot.atCurso.model.Questionary;
 import com.fot.atCurso.model.User;
@@ -12,6 +13,6 @@ import com.fot.atCurso.service.AbstractService;
 
 public interface CourseService  extends AbstractService<Course, Integer> {
 
-	public Set<User> findUsers(Pageable p);
-	public Set<Questionary> findQuestionaries(Pageable p);
+	public Set<User> findCourseUsers(Pageable p, Integer idCourse) throws NotFoundException;
+	public Set<Questionary> findCourseQuestionaries(Pageable p, Integer idCourse) throws NotFoundException;
 }
