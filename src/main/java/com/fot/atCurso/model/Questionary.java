fc7ebc61b094698710a902227f675fea31ca60ff
Orrequia <orrequia8@gmail.com>
2018-05-28 12:54:32 +0200

    Eliminado problema de los id's independientes y cambiada la bidireccionalidad con Course

--- src/main/java/com/fot/atCurso/model/Questionary.java
+++ src/main/java/com/fot/atCurso/model/Questionary.java
@@ -3,15 +3,21 @@ package com.fot.atCurso.model;
 
 import java.util.List;
 
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+
+import com.fot.atCurso.enums.ModalityEnum;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,21 +29,22 @@ import lombok.Setter;
 @Entity
 public class Questionary {
 	
-	public static final String FIELD_COURSE = "course";
 	public static final String FIELD_IDTAG = "idTag";
 	public static final String FIELD_IDQUESTIONARY = "idQuestionary";
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name=FIELD_IDQUESTIONARY)
 	private Integer idQuestionary;
 	
 	@Column(nullable = false)
 	private String name;
 	
-	@JoinColumn(name = FIELD_COURSE)
-	@ManyToOne(fetch = FetchType.LAZY)
-	private Course course;
+	@Column(nullable = false)
+	private Integer nQuestion;
+	
+	@Enumerated(EnumType.STRING)
+	private ModalityEnum modality;
 	
 	@ManyToMany(fetch = FetchType.LAZY)
 	@JoinTable(
