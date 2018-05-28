20e91e93e5ae4120b8d9c20308f896d3b75f5722
Orrequia <orrequia8@gmail.com>
2018-05-28 13:00:34 +0200

    Añadido 2 metodos para obtener los usuarios y los cuestionarios

--- src/main/java/com/fot/atCurso/service/course/CourseService.java
+++ src/main/java/com/fot/atCurso/service/course/CourseService.java
@@ -1,8 +1,17 @@
 package com.fot.atCurso.service.course;
 
+import java.util.Set;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
 import com.fot.atCurso.model.Course;
+import com.fot.atCurso.model.Questionary;
+import com.fot.atCurso.model.User;
 import com.fot.atCurso.service.AbstractService;
 
 public interface CourseService  extends AbstractService<Course, Integer> {
 
+	public Set<User> findUsers(Pageable p);
+	public Set<Questionary> findQuestionaries(Pageable p);
 }
