e54ba85a2f27dada140035845edf9c3c9a0e8a5e
Orrequia <orrequia8@gmail.com>
2018-05-28 12:55:27 +0200

    Creado interfaz del mapper de Questionary

---
+++ src/main/java/com/fot/atCurso/component/mapper/questionary/QuestionaryMapper.java
@@ -0,0 +1,9 @@
+package com.fot.atCurso.component.mapper.questionary;
+
+import com.fot.atCurso.component.mapper.Mapper;
+import com.fot.atCurso.dto.questionary.QuestionaryDTO;
+import com.fot.atCurso.model.Questionary;
+
+public interface QuestionaryMapper extends Mapper<Questionary, QuestionaryDTO> {
+
+}
