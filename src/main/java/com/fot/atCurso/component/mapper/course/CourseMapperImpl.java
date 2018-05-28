16dc94dea349ddadb1fda84bc3692a5efb76be1c
Orrequia <orrequia8@gmail.com>
2018-05-28 13:18:11 +0200

    Añadido @Component

--- src/main/java/com/fot/atCurso/component/mapper/course/CourseMapperImpl.java
+++ src/main/java/com/fot/atCurso/component/mapper/course/CourseMapperImpl.java
@@ -1,9 +1,12 @@
 package com.fot.atCurso.component.mapper.course;
 
+import org.springframework.stereotype.Component;
+
 import com.fot.atCurso.component.mapper.AbstractMapper;
 import com.fot.atCurso.dto.course.CourseDTO;
 import com.fot.atCurso.model.Course;
 
+@Component
 public class CourseMapperImpl extends AbstractMapper<Course, CourseDTO> implements CourseMapper {
 
 	@Override
