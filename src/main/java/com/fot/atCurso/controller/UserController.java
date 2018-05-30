d5e5af075869a21ba1b079dee75df6a22b29b69a
Orrequia <orrequia8@gmail.com>
2018-05-30 12:18:47 +0200

    Eliminada algunas excepciones y bajarla al service o mapper

--- src/main/java/com/fot/atCurso/controller/UserController.java
+++ src/main/java/com/fot/atCurso/controller/UserController.java
@@ -3,6 +3,7 @@ package com.fot.atCurso.controller;
 import java.util.Optional;
 import java.util.Set;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,15 +20,12 @@ import com.fot.atCurso.exceptions.IdValueCannotBeReceivedException;
 import com.fot.atCurso.exceptions.NotFoundException;
 import com.fot.atCurso.exceptions.ObjectsDoNotMatchException;
 import com.fot.atCurso.exceptions.ParametersNotAllowedException;
-import com.fot.atCurso.exceptions.UniqueValueViolationException;
 import com.fot.atCurso.model.User;
 import com.fot.atCurso.service.user.UserService;
  
 @RestController
 @RequestMapping(value= "/user")
 public class UserController {
-
-	private static final Integer maxSize = new Integer(10);
 	
 	@Autowired
 	UserService userService;
@@ -38,8 +36,6 @@ public class UserController {
 	@RequestMapping(method = RequestMethod.GET)
 	public Set<UserDTO> findAll(@RequestParam(defaultValue = "0", required= false ) Integer page, 
 							 @RequestParam(defaultValue = "10", required= false ) Integer size) throws ParametersNotAllowedException {
-		if(page < 0 || size <= 0 || size > maxSize)
-			throw new ParametersNotAllowedException("Los parámetros introducidos contienen valores no permitidos, page mayor o igual a 0 y size entre 1 y " + maxSize + " incluídos");
 		final Set<User> users = userService.findAll(PageRequest.of(page, size));
 		return userMapper.modelToDto(users);
 	}
@@ -52,24 +48,20 @@ public class UserController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public UserDTO create(@RequestBody UserPostDTO dto) throws IdValueCannotBeReceivedException, UniqueValueViolationException {
+	public UserDTO create(@RequestBody UserPostDTO dto) throws IdValueCannotBeReceivedException, ConstraintViolationException, NotFoundException {
 		if(dto.getIdUser() != null) 
 			throw new IdValueCannotBeReceivedException("El idUser no se puede recibir");
-		if(userService.findByEmail(dto.getEmail()).isPresent())
-			throw new UniqueValueViolationException("El email introducido ya existe");
 		final User user = userMapper.dtoToModel(dto);
 		final User createUser = userService.create(user);
 		return userMapper.modelToDto(createUser);
 	}
 	
 	@RequestMapping(value = "/{idUser}", method = RequestMethod.PUT)
-	public void update(@PathVariable("idUser") Integer id, @RequestBody UserPostDTO dto) throws IdValueCannotBeReceivedException, NotFoundException, UniqueValueViolationException {
+	public void update(@PathVariable("idUser") Integer id, @RequestBody UserPostDTO dto) throws IdValueCannotBeReceivedException, NotFoundException {
 		if(dto.getIdUser() != null) 
 			throw new IdValueCannotBeReceivedException("El idUser no se puede recibir en el body");
 		final Optional<User> user = userService.findById(id);
 		user.orElseThrow(() -> new NotFoundException("El usuario no existe"));
-		if (user.get().getEmail() != dto.getEmail() && userService.findByEmail(dto.getEmail()).isPresent()) 
-			throw new UniqueValueViolationException("El email a cambiar ya existe");
 		userService.setValues(user.get(), userMapper.dtoToModel(dto));
 		userService.update(user.get());
 	}
