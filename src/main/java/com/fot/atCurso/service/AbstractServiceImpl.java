3e91414d9a6a7fe5a3504bbc3cae9708938230c7
Orrequia <orrequia8@gmail.com>
2018-05-30 12:18:08 +0200

    implementada la excepción para controlar page y size

--- src/main/java/com/fot/atCurso/service/AbstractServiceImpl.java
+++ src/main/java/com/fot/atCurso/service/AbstractServiceImpl.java
@@ -10,10 +10,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.fot.atCurso.dao.GenericDAO;
+import com.fot.atCurso.exceptions.ParametersNotAllowedException;
 
 @Service
 public abstract class AbstractServiceImpl<T, D extends GenericDAO<T>> implements AbstractService<T, Integer> {
 
+	private static final Integer maxSize = new Integer(10);
+	
 	@Autowired
 	D dao;
 	
@@ -33,7 +36,9 @@ public abstract class AbstractServiceImpl<T, D extends GenericDAO<T>> implements
 	}
 
 	@Override
-	public Set<T> findAll(Pageable p) {
+	public Set<T> findAll(Pageable p) throws ParametersNotAllowedException {
+		if(p.getPageNumber() < 0 || p.getPageSize() <= 0 || p.getPageSize() > maxSize)
+			throw new ParametersNotAllowedException("Los parámetros introducidos contienen valores no permitidos, page mayor o igual a 0 y size entre 1 y " + maxSize + " incluídos");
 		return dao.findAll(PageRequest.of(p.getPageNumber(), p.getPageSize())).stream().collect(Collectors.toSet());
 	}
 
