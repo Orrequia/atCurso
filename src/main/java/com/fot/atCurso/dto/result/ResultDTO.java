3bf6a06581b28b6dbe3c96d4bf05a8bc519afa49
Orrequia <orrequia8@gmail.com>
2018-05-31 10:26:01 +0200

    Creado el DTO de resultados

---
+++ src/main/java/com/fot/atCurso/dto/result/ResultDTO.java
@@ -0,0 +1,15 @@
+package com.fot.atCurso.dto.result;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class ResultDTO {
+
+	private Integer idResult;
+	private Date date;
+	private Float score;
+	private List<Integer> quizzes;
+}
