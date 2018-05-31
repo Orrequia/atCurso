ceec9305649c2cf345494bf2ad4312e5d3abe382
Orrequia <orrequia8@gmail.com>
2018-05-31 10:08:57 +0200

    Modificado para que los métodos dtoToModel pueda lanzar excepcion

--- src/main/java/com/fot/atCurso/component/mapper/Mapper.java
+++ src/main/java/com/fot/atCurso/component/mapper/Mapper.java
@@ -2,13 +2,15 @@ package com.fot.atCurso.component.mapper;
 
 import java.util.Set;
 
+import com.fot.atCurso.exceptions.NotFoundException;
+
 public interface Mapper<M, D> {
 	
-	M dtoToModel(D dto);
+	M dtoToModel(D dto) throws NotFoundException;
 	
 	D modelToDto(M model);
 	
-	Set<M> dtoToModel(Set<D> dtos);
+	Set<M> dtoToModel(Set<D> dtos) throws NotFoundException;
 	
 	Set<D> modelToDto(Set<M> models);
 	
