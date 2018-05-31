eb7297aef46ed2066c295fe861caf748ac07e9e5
Orrequia <orrequia8@gmail.com>
2018-05-31 10:10:33 +0200

    Corregido error gramatical en el mensaje por defecto

--- src/main/java/com/fot/atCurso/exceptions/ParametersNotAllowedException.java
+++ src/main/java/com/fot/atCurso/exceptions/ParametersNotAllowedException.java
@@ -3,7 +3,7 @@ package com.fot.atCurso.exceptions;
 public class ParametersNotAllowedException extends Exception {
 	private static final long serialVersionUID = 789876787L;
 
-	public static final String MSG = "Los valors de los parámetros no están permitidos";
+	public static final String MSG = "Los valores de los parámetros no están permitidos";
 	
 	public ParametersNotAllowedException(String message) {
 		super(message);
