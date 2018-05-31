27508831d17e3d818b85785e23c869db06938429
Orrequia <orrequia8@gmail.com>
2018-05-31 10:10:05 +0200

    Modificado mensaje por defecto

--- src/main/java/com/fot/atCurso/exceptions/ParametersNotAllowedException.java
+++ src/main/java/com/fot/atCurso/exceptions/ParametersNotAllowedException.java
@@ -3,7 +3,7 @@ package com.fot.atCurso.exceptions;
 public class ParametersNotAllowedException extends Exception {
 	private static final long serialVersionUID = 789876787L;
 
-	public static final String MSG = "El id no se puede recibir";
+	public static final String MSG = "Los valors de los parámetros no están permitidos";
 	
 	public ParametersNotAllowedException(String message) {
 		super(message);
