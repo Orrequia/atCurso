9efb99f4163a3ee584a9db49db185a7f0a0b2710
Orrequia <orrequia8@gmail.com>
2018-05-31 10:12:44 +0200

    Modificado Questioniary por Quiz

--- src/main/java/com/fot/atCurso/service/course/CourseService.java
+++ src/main/java/com/fot/atCurso/service/course/CourseService.java
@@ -7,12 +7,12 @@ import org.springframework.data.domain.Pageable;
 
 import com.fot.atCurso.exceptions.NotFoundException;
 import com.fot.atCurso.model.Course;
-import com.fot.atCurso.model.Questionary;
+import com.fot.atCurso.model.Quiz;
 import com.fot.atCurso.model.User;
 import com.fot.atCurso.service.AbstractService;
 
 public interface CourseService  extends AbstractService<Course, Integer> {
 
 	public Set<User> findCourseUsers(Pageable p, Integer idCourse) throws NotFoundException;
-	public Set<Questionary> findCourseQuestionaries(Pageable p, Integer idCourse) throws NotFoundException;
+	public Set<Quiz> findCourseQuestionaries(Pageable p, Integer idCourse) throws NotFoundException;
 }
