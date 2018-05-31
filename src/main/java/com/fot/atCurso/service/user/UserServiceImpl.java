b2c60c77e14a44b0372ca930cb162e547ff24c4d
Orrequia <orrequia8@gmail.com>
2018-05-31 11:06:12 +0200

    Implementado el nuevo método para añadir un resultado a la lista de resultados del usuario

--- src/main/java/com/fot/atCurso/service/user/UserServiceImpl.java
+++ src/main/java/com/fot/atCurso/service/user/UserServiceImpl.java
@@ -6,14 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fot.atCurso.dao.UserDAO;
+import com.fot.atCurso.model.Result;
 import com.fot.atCurso.model.User;
 import com.fot.atCurso.service.AbstractServiceImpl;
+import com.fot.atCurso.service.result.ResultService;
 
 @Service
 public class UserServiceImpl extends AbstractServiceImpl<User, UserDAO> implements UserService {
 	
 	@Autowired
 	UserDAO userDAO;
+	
+	@Autowired
+	ResultService resultService;
 
 	@Override
 	public boolean isEqual(User u1, User u2) {
@@ -33,5 +38,11 @@ public class UserServiceImpl extends AbstractServiceImpl<User, UserDAO> implemen
 	@Override
 	public Optional<User> findByEmail(String email) {
 		return userDAO.findOneByEmail(email);
+	}
+	
+	@Override
+	public void addResult(User user, Result result) {
+		final Result createResult = resultService.create(result);
+		user.getResult().add(createResult);
 	}
 }
