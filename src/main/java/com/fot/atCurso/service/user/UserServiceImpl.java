a327933b7a1a48c947bc95ee8956d57e8f2f8b73
Orrequia <orrequia8@gmail.com>
2018-05-31 10:06:53 +0200

    Añadido comparación con Resultados e insertando los resultados anteriores

--- src/main/java/com/fot/atCurso/service/user/UserServiceImpl.java
+++ src/main/java/com/fot/atCurso/service/user/UserServiceImpl.java
@@ -1,14 +1,11 @@
 package com.fot.atCurso.service.user;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fot.atCurso.dao.UserDAO;
-import com.fot.atCurso.model.Result;
 import com.fot.atCurso.model.User;
 import com.fot.atCurso.service.AbstractServiceImpl;
 
@@ -21,24 +18,20 @@ public class UserServiceImpl extends AbstractServiceImpl<User, UserDAO> implemen
 	@Override
 	public boolean isEqual(User u1, User u2) {
 		return u1.getName().equals( u2.getName()) &&
-				u1.getEmail().equals(u2.getEmail());
+				u1.getEmail().equals(u2.getEmail()) &&
+				u1.getResult().equals(u2.getResult());
 	}
 	
 	@Override
 	public void setValues(User to, User from) {
-		if(from.getName() != null) to.setName(from.getName());
-		if(from.getEmail() != null) to.setEmail(from.getEmail());
-		if(from.getPassword() != null) to.setPassword(from.getPassword());
+		to.setName(from.getName());
+		to.setEmail(from.getEmail());
+		to.setPassword(from.getPassword());
+		to.setResult(from.getResult());
 	}
 	
 	@Override
 	public Optional<User> findByEmail(String email) {
 		return userDAO.findOneByEmail(email);
-	}
-	
-	@Override
-	public List<Result> findResults(User user) {
-		//return userDAO.
-		return new ArrayList<Result>();
 	}
 }
