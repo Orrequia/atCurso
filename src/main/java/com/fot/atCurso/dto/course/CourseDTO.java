42bbcd12157ee6829d11ea3530520020e441cfcd
Orrequia <orrequia8@gmail.com>
2018-05-31 10:09:31 +0200

    Añadido la lista de usuarios y cuestionarios

--- src/main/java/com/fot/atCurso/dto/course/CourseDTO.java
+++ src/main/java/com/fot/atCurso/dto/course/CourseDTO.java
@@ -1,6 +1,10 @@
 package com.fot.atCurso.dto.course;
 
 import java.util.Date;
+import java.util.List;
+
+import com.fot.atCurso.model.Quiz;
+import com.fot.atCurso.model.User;
 
 import lombok.Data;
 
@@ -11,4 +15,6 @@ public class CourseDTO {
 	private String name;
 	private Date fecha_inicio;
 	private Date fecha_fin;
+	private List<Integer> users;
+	private List<Integer> quizzes;
 }
