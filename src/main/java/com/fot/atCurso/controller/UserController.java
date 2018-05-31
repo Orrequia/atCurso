40e0aa4fcac0154960975d756eee6cb023c65e2e
Orrequia <orrequia8@gmail.com>
2018-05-31 10:16:03 +0200

    Cambiado los Mapping a la última version de Spring

--- src/main/java/com/fot/atCurso/controller/UserController.java
+++ src/main/java/com/fot/atCurso/controller/UserController.java
@@ -6,7 +6,11 @@ import java.util.Set;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,21 +37,21 @@ public class UserController {
 	@Autowired
 	UserMapper userMapper;
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public Set<UserDTO> findAll(@RequestParam(defaultValue = "0", required= false ) Integer page, 
 							 @RequestParam(defaultValue = "10", required= false ) Integer size) throws ParametersNotAllowedException {
 		final Set<User> users = userService.findAll(PageRequest.of(page, size));
 		return userMapper.modelToDto(users);
 	}
 	
-	@RequestMapping(value = "/{idUser}", method = RequestMethod.GET)
+	@GetMapping("/{idUser}")
 	public UserDTO findById(@PathVariable("idUser") Integer id) throws NotFoundException {
 		final Optional<User> user = userService.findById(id);
 		if (user.isPresent()) return userMapper.modelToDto(user.get());
 		throw new NotFoundException("El usuario no existe");
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
+	@PostMapping
 	public UserDTO create(@RequestBody UserPostDTO dto) throws IdValueCannotBeReceivedException, ConstraintViolationException, NotFoundException {
 		if(dto.getIdUser() != null) 
 			throw new IdValueCannotBeReceivedException("El idUser no se puede recibir");
@@ -56,7 +60,7 @@ public class UserController {
 		return userMapper.modelToDto(createUser);
 	}
 	
-	@RequestMapping(value = "/{idUser}", method = RequestMethod.PUT)
+	@PutMapping("/{idUser}")
 	public void update(@PathVariable("idUser") Integer id, @RequestBody UserPostDTO dto) throws IdValueCannotBeReceivedException, NotFoundException {
 		if(dto.getIdUser() != null) 
 			throw new IdValueCannotBeReceivedException("El idUser no se puede recibir en el body");
@@ -66,7 +70,7 @@ public class UserController {
 		userService.update(user.get());
 	}
 	
-	@RequestMapping(value = "/{idUser}", method = RequestMethod.DELETE)
+	@DeleteMapping("/{idUser}")
 	public void delete(@PathVariable("idUser") Integer id, @RequestBody UserDTO dto) throws NotFoundException, ObjectsDoNotMatchException {
 		final Optional<User> user = userService.findById(id);
 		user.orElseThrow(() -> new NotFoundException("El usuario no existe"));
