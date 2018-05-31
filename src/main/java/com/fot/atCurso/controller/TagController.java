e37a413d319f4dd3f7be2e4890e4187a546b43dd
Orrequia <orrequia8@gmail.com>
2018-05-31 14:15:58 +0200

    Creado CRD de Tag

--- src/main/java/com/fot/atCurso/controller/TagController.java
+++ src/main/java/com/fot/atCurso/controller/TagController.java
@@ -1,13 +1,68 @@
 package com.fot.atCurso.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import java.util.Optional;
+import java.util.Set;
 
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fot.atCurso.component.mapper.tag.TagMapper;
+import com.fot.atCurso.dto.tag.TagDTO;
+import com.fot.atCurso.exceptions.IdValueCannotBeReceivedException;
+import com.fot.atCurso.exceptions.NotFoundException;
+import com.fot.atCurso.exceptions.ObjectsDoNotMatchException;
+import com.fot.atCurso.exceptions.ParametersNotAllowedException;
+import com.fot.atCurso.model.Tag;
 import com.fot.atCurso.service.tag.TagService;
 
-@Controller
+@RestController
+@RequestMapping(value= "/tag")
 public class TagController {
-
+	
 	@Autowired
 	TagService tagService;
+	
+	@Autowired
+	TagMapper tagMapper;
+	
+	@GetMapping
+	public Set<TagDTO> findAll(@RequestParam(defaultValue = "0", required= false ) Integer page, 
+							 @RequestParam(defaultValue = "10", required= false ) Integer size) throws ParametersNotAllowedException {
+		final Set<Tag> tags = tagService.findAll(PageRequest.of(page, size));
+		return tagMapper.modelToDto(tags);
+	}
+	
+	@GetMapping("/{idTag}")
+	public TagDTO findById(@PathVariable("idTag") Integer id) throws NotFoundException {
+		final Optional<Tag> tag = tagService.findById(id);
+		if (tag.isPresent()) return tagMapper.modelToDto(tag.get());
+		throw new NotFoundException("El tag no existe");
+	}
+	
+	@PostMapping
+	public TagDTO create(@RequestBody TagDTO dto) throws IdValueCannotBeReceivedException, ConstraintViolationException, NotFoundException {
+		if(dto.getIdTag() != null) 
+			throw new IdValueCannotBeReceivedException("El idTag no se puede recibir");
+		final Tag tag = tagMapper.dtoToModel(dto);
+		final Tag createTag = tagService.create(tag);
+		return tagMapper.modelToDto(createTag);
+	}
+	
+	@DeleteMapping("/{idTag}")
+	public void delete(@PathVariable("idTag") Integer id, @RequestBody TagDTO dto) throws NotFoundException, ObjectsDoNotMatchException {
+		final Optional<Tag> tag = tagService.findById(id);
+		tag.orElseThrow(() -> new NotFoundException("El tag no existe"));
+		if(!tagService.isEqual(tagMapper.dtoToModel(dto), tag.get())) 
+			throw new ObjectsDoNotMatchException("El tag recibido no coincide con el almacenado");
+		tagService.delete(tag.get());
+	}
 }
