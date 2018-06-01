7bf90101ea604d84e5e782ca1724bdf2191867b0
Orrequia <orrequia8@gmail.com>
2018-06-01 14:52:52 +0200

    ELiminado el servicio de curso y realizado las modificaciones oportunas

--- src/main/java/com/fot/atCurso/controller/CourseQuizController.java
+++ src/main/java/com/fot/atCurso/controller/CourseQuizController.java
@@ -1,12 +1,9 @@
 package com.fot.atCurso.controller;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.List;
 
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,16 +22,11 @@ import com.fot.atCurso.exceptions.NotFoundException;
 import com.fot.atCurso.exceptions.ObjectsDoNotMatchException;
 import com.fot.atCurso.exceptions.ParametersNotAllowedException;
 import com.fot.atCurso.model.Quiz;
-import com.fot.atCurso.service.course.CourseService;
 import com.fot.atCurso.service.quiz.QuizService;
-import com.fot.atCurso.model.Course;
 
 @RestController
 @RequestMapping(value= "/course/{idCourse}/quiz")
 public class CourseQuizController {
-	
-	@Autowired
-	CourseService courseService;
 	
 	@Autowired
 	QuizService quizService;
@@ -43,24 +35,18 @@ public class CourseQuizController {
 	QuizMapper quizMapper;
 	
 	@GetMapping
-	public Set<QuizDTO> findAll(@RequestParam(defaultValue = "0", required= false ) Integer page, 
+	public List<QuizDTO> findAll(@RequestParam(defaultValue = "0", required= false ) Integer page, 
 							 @RequestParam(defaultValue = "10", required= false ) Integer size,
 							 @PathVariable("idCourse") Integer idCourse) throws ParametersNotAllowedException, NotFoundException {
-		final Optional<Course> course = courseService.findById(idCourse);
-		course.orElseThrow(() -> new NotFoundException("El curso no existe"));
-		final Set<Quiz> quizs = new PageImpl<Quiz>(course.get().getQuiz(), PageRequest.of(page, size), course.get().getQuiz().size())
-				.stream().collect(Collectors.toSet());
-		return quizMapper.modelToDto(quizs);
+		final List<Quiz> results = quizService.findQuizByCourse(idCourse, PageRequest.of(page, size));
+		return quizMapper.modelToDto(results);
 	}
 	
 	@GetMapping("/{idQuiz}")
 	public QuizDTO findById(@PathVariable("idCourse") Integer idCourse,
 			 @PathVariable("idQuiz") Integer idQuiz) throws NotFoundException {
-		final Optional<Course> course = courseService.findById(idCourse);
-		course.orElseThrow(() -> new NotFoundException("El usuario no existe"));
-		final Optional<Quiz> quiz = courseService.searchQuiz(course.get(), idQuiz);
-		course.orElseThrow(() -> new NotFoundException("Este quizado no existe para este usuario"));
-		return quizMapper.modelToDto(quiz.get());
+		final Quiz result = quizService.findOneQuizByCourse(idCourse, idQuiz);
+		return quizMapper.modelToDto(result);
 	}
 	
 	@PostMapping
@@ -68,10 +54,7 @@ public class CourseQuizController {
 			@PathVariable("idCourse") Integer idCourse) throws IdValueCannotBeReceivedException, ConstraintViolationException, NotFoundException {
 		if(dto.getIdQuiz() != null) 
 			throw new IdValueCannotBeReceivedException("El idQuiz no se puede recibir");
-		final Optional<Course> course = courseService.findById(idCourse);
-		course.orElseThrow(() -> new NotFoundException("El usuario no existe"));
-		Quiz createQuiz = quizService.create(quizMapper.dtoToModel(dto));
-		courseService.addQuiz(course.get(), createQuiz);
+		Quiz createQuiz = quizService.addToCourse(idCourse, quizMapper.dtoToModel(dto));
 		return quizMapper.modelToDto(createQuiz);
 	}
 	
@@ -81,25 +64,13 @@ public class CourseQuizController {
 		    @RequestBody QuizDTO dto) throws IdValueCannotBeReceivedException, NotFoundException {
 		if(dto.getIdQuiz() != null) 
 			throw new IdValueCannotBeReceivedException("El idQuiz no se puede recibir");
-		final Optional<Course> course = courseService.findById(idCourse);
-		course.orElseThrow(() -> new NotFoundException("El usuario no existe"));
-		final Optional<Quiz> quiz = courseService.searchQuiz(course.get(), idQuiz);
-		quiz.orElseThrow(() -> new NotFoundException("Este quizado no existe para este usuario"));
-		quizService.setValues(quiz.get(), quizMapper.dtoToModel(dto));
-		quizService.update(quiz.get());
+		quizService.updateToCourse(idCourse, idQuiz, quizMapper.dtoToModel(dto));
 	}
 	
 	@DeleteMapping("/{idQuiz}")
 	public void delete(@PathVariable("idCourse") Integer idCourse,
 			@PathVariable("idQuiz") Integer idQuiz, 
 			@RequestBody QuizDTO dto) throws NotFoundException, ObjectsDoNotMatchException {
-		final Optional<Course> course = courseService.findById(idCourse);
-		course.orElseThrow(() -> new NotFoundException("El usuario no existe"));
-		final Optional<Quiz> quiz = courseService.searchQuiz(course.get(), idQuiz);
-		quiz.orElseThrow(() -> new NotFoundException("Este quizado no existe para este usuario"));
-		if(!quizService.isEqual(quizMapper.dtoToModel(dto), quiz.get())) 
-			throw new ObjectsDoNotMatchException("El quizado recibido no coincide con el almacenado");
-		courseService.removeQuiz(course.get(), quiz.get());
-		quizService.delete(quiz.get());
+		quizService.deleteToCourse(idCourse, idQuiz, quizMapper.dtoToModel(dto));
 	}
 }
