ea3fc1f6bef62de738850df3e9545fb0a95e8cca
Orrequia <orrequia8@gmail.com>
2018-06-01 15:22:37 +0200

    Sobreescrito los metodos para que transforme los AnswerDTO

--- src/main/java/com/fot/atCurso/component/mapper/question/QuestionMapperImpl.java
+++ src/main/java/com/fot/atCurso/component/mapper/question/QuestionMapperImpl.java
@@ -1,17 +1,21 @@
 package com.fot.atCurso.component.mapper.question;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.fot.atCurso.component.mapper.AbstractMapper;
-import com.fot.atCurso.component.mapper.course.CourseMapper;
-import com.fot.atCurso.dto.course.CourseDTO;
+import com.fot.atCurso.component.mapper.answer.AnswerMapper;
 import com.fot.atCurso.dto.question.QuestionDTO;
-import com.fot.atCurso.model.Course;
+import com.fot.atCurso.exceptions.NotFoundException;
 import com.fot.atCurso.model.Question;
+
 
 @Component
 public class QuestionMapperImpl extends AbstractMapper<Question, QuestionDTO> implements QuestionMapper {
 
+	@Autowired
+	AnswerMapper answerMapper;
+	
 	@Override
 	public Class<? extends QuestionDTO> dtoClazz() {
 		return QuestionDTO.class;
@@ -20,5 +24,19 @@ public class QuestionMapperImpl extends AbstractMapper<Question, QuestionDTO> im
 	@Override
 	public Class<? extends Question> modelClazz() {
 		return Question.class;
+	}
+	
+	@Override
+	public Question dtoToModel(QuestionDTO dto) throws NotFoundException {
+		Question question = dozer.map(dto, modelClazz());
+		question.setAnswer(answerMapper.dtoToModel(dto.getAnwers()));
+		return question;
+	}
+	
+	@Override
+	public QuestionDTO modelToDto(Question model) {
+		QuestionDTO questionDTO = dozer.map(model, dtoClazz());
+		questionDTO.setAnwers(answerMapper.modelToDto(model.getAnswer()));
+		return questionDTO;
 	}
 }
