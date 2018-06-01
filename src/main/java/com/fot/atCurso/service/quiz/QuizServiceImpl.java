48e982423e50d9734a1ac09ab38eb2fc8be6894a
Orrequia <orrequia8@gmail.com>
2018-06-01 14:51:40 +0200

    implementado los métodos para obtener, añadir, modificar y eliminar los cuestionarios de un curso.

--- src/main/java/com/fot/atCurso/service/quiz/QuizServiceImpl.java
+++ src/main/java/com/fot/atCurso/service/quiz/QuizServiceImpl.java
@@ -5,14 +5,19 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.fot.atCurso.dao.QuizDAO;
 import com.fot.atCurso.exceptions.NotFoundException;
+import com.fot.atCurso.exceptions.ObjectsDoNotMatchException;
 import com.fot.atCurso.model.Course;
 import com.fot.atCurso.model.Quiz;
+import com.fot.atCurso.model.Result;
+import com.fot.atCurso.model.Quiz;
 import com.fot.atCurso.model.Tag;
-import com.fot.atCurso.model.User;
+import com.fot.atCurso.model.Course;
 import com.fot.atCurso.service.AbstractServiceImpl;
 import com.fot.atCurso.service.course.CourseService;
 import com.fot.atCurso.service.tag.TagService;
@@ -25,6 +30,9 @@ public class QuizServiceImpl extends AbstractServiceImpl<Quiz, QuizDAO> implemen
 	
 	@Autowired
 	TagService tagService;
+	
+	@Autowired
+	CourseService courseService;
 	
 	@Override
 	public boolean isEqual(Quiz q1, Quiz q2) {
@@ -40,5 +48,51 @@ public class QuizServiceImpl extends AbstractServiceImpl<Quiz, QuizDAO> implemen
 		to.setModality(from.getModality());
 		to.setQuestion(from.getQuestion());
 		to.setTag(from.getTag());
+	}
+	
+	@Override
+	public List<Quiz> findQuizByCourse(Integer idCourse, Pageable p) throws NotFoundException {
+		courseService.getAndCheck(idCourse);;
+		return quizDAO.findByCourse(idCourse, PageRequest.of(p.getPageNumber(), p.getPageSize()));
+	}
+	
+	@Override
+	public Quiz findOneQuizByCourse(Integer idCourse, Integer idQuiz) throws NotFoundException {
+		final Course course = courseService.getAndCheck(idCourse);
+		final Quiz quiz = getAndCheckBelongCourse(course, idQuiz);
+		return quiz;
+	}
+
+	@Override
+	public Quiz addToCourse(Integer idCourse, Quiz quiz) throws NotFoundException {
+		final Course course = courseService.getAndCheck(idCourse);
+		final Quiz createQuiz = create(quiz);
+		courseService.addQuiz(course, createQuiz);
+		return createQuiz;
+	}
+	
+	@Override
+	public void updateToCourse(Integer idCourse, Integer idQuiz, Quiz newQuiz) throws NotFoundException {
+		final Course course = courseService.getAndCheck(idCourse);
+		final Quiz quiz = getAndCheckBelongCourse(course, idQuiz);
+		setValues(quiz, newQuiz);
+		update(quiz);
+	}
+	
+	@Override
+	public void deleteToCourse(Integer idCourse, Integer idQuiz, Quiz bodyQuiz) throws NotFoundException, ObjectsDoNotMatchException {
+		final Course course = courseService.getAndCheck(idCourse);
+		final Quiz quiz = getAndCheckBelongCourse(course, idQuiz);
+		if(!isEqual(bodyQuiz, quiz))
+			throw new ObjectsDoNotMatchException("El quizado recibido no coincide con el almacenado");
+		courseService.removeQuiz(course, quiz);
+		delete(quiz);
+	}
+	
+	@Override
+	public Quiz getAndCheckBelongCourse(Course course, Integer idQuiz) throws NotFoundException {
+		final Optional<Quiz> quiz = course.getQuiz().stream().filter(q -> q.getIdQuiz() == idQuiz).findFirst();
+		quiz.orElseThrow(() -> new NotFoundException("Este cuestionario no existe para este curso"));
+		return quiz.get();
 	}
 }
