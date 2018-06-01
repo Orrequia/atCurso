1e94b64872bdf5aa597a7aa706e5559711b28a2e
Orrequia <orrequia8@gmail.com>
2018-06-01 13:55:19 +0200

    Eliminado el courseService y añadido userService

--- src/main/java/com/fot/atCurso/controller/CourseUserController.java
+++ src/main/java/com/fot/atCurso/controller/CourseUserController.java
@@ -1,11 +1,8 @@
 package com.fot.atCurso.controller;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,37 +13,30 @@ import com.fot.atCurso.component.mapper.user.UserMapper;
 import com.fot.atCurso.dto.user.UserDTO;
 import com.fot.atCurso.exceptions.NotFoundException;
 import com.fot.atCurso.exceptions.ParametersNotAllowedException;
-import com.fot.atCurso.model.Course;
 import com.fot.atCurso.model.User;
-import com.fot.atCurso.service.course.CourseService;
+import com.fot.atCurso.service.user.UserService;
 
 @RequestMapping(value="course/{idCourse}/user")
 public class CourseUserController {
 	
 	@Autowired
-	CourseService courseService;
+	UserService userService;
 	
 	@Autowired
 	UserMapper userMapper;
 	
 	@GetMapping
-	public Set<UserDTO> findAll(@RequestParam(defaultValue = "0", required= false ) Integer page, 
-							 @RequestParam(defaultValue = "10", required= false ) Integer size,
+	public List<UserDTO> findAll(@RequestParam(defaultValue = "0", required= false) Integer page, 
+							 @RequestParam(defaultValue = "10", required= false) Integer size,
 							 @PathVariable("idCourse") Integer idCourse) throws ParametersNotAllowedException, NotFoundException {
-		final Optional<Course> course = courseService.findById(idCourse);
-		course.orElseThrow(() -> new NotFoundException("El curso no existe"));
-		final Set<User> users = new PageImpl<User>(course.get().getUser(), PageRequest.of(page, size), course.get().getUser().size())
-				.stream().collect(Collectors.toSet());
+		final List<User> users = userService.findUserByCourse(idCourse, PageRequest.of(page, size));
 		return userMapper.modelToDto(users);
 	}
 	
 	@GetMapping("/{idUser}")
 	public UserDTO findById(@PathVariable("idCourse") Integer idCourse,
 			 @PathVariable("idUser") Integer idUser) throws NotFoundException {
-		final Optional<Course> course = courseService.findById(idCourse);
-		course.orElseThrow(() -> new NotFoundException("El curso no existe"));
-		final Optional<User> user = courseService.searchUser(course.get(), idUser);
-		user.orElseThrow(() -> new NotFoundException("Este usuario no pertenece a este curso"));
-		return userMapper.modelToDto(user.get());
+		final User user = userService.findOneUserByCourse(idCourse, idUser);
+		return userMapper.modelToDto(user);
 	}
 }
