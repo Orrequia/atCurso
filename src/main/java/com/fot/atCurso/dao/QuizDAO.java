5d7a45af151f0d8bffc85c1320d5b8ad2c5347b1
Orrequia <orrequia8@gmail.com>
2018-06-01 14:50:41 +0200

    Añadido consulta para obtener los cuestionarios de un curso

--- src/main/java/com/fot/atCurso/dao/QuizDAO.java
+++ src/main/java/com/fot/atCurso/dao/QuizDAO.java
@@ -1,10 +1,18 @@
 package com.fot.atCurso.dao;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fot.atCurso.model.Quiz;
+import com.fot.atCurso.model.Result;
 
 @Repository
 public interface QuizDAO extends GenericDAO<Quiz> {
 
+	@Query(value = "SELECT FROM Quiz quiz WHERE quiz.idCourse = :idCourse ORDER BY quiz.name")
+	List<Result> findByUser(@Param("idCourse") Integer idCourse, Pageable p);
 }
