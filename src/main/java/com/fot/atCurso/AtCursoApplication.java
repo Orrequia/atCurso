e0abc0ae1981ceb50b4a694960c9e683597ffccb
Orrequia <orrequia8@gmail.com>
2018-06-01 09:22:21 +0200

    Añadida etiqueta para activar la configuración automática.

--- src/main/java/com/fot/atCurso/AtCursoApplication.java
+++ src/main/java/com/fot/atCurso/AtCursoApplication.java
@@ -1,9 +1,11 @@
 package com.fot.atCurso;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class AtCursoApplication {
 
 	public static void main(String[] args) {
