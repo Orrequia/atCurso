7e4fc5085c69ca3d6515775d5138016338809a8b
Orrequia <orrequia8@gmail.com>
2018-06-01 22:42:19 +0200

    Creado componente para el tratamiento de fechas, añadido método compare

---
+++ src/main/java/com/fot/atCurso/component/dates/OperationDates.java
@@ -0,0 +1,8 @@
+package com.fot.atCurso.component.dates;
+
+import java.util.Date;
+
+public interface OperationDates {
+
+	boolean compare(Date d1, Date d2);
+}
---
+++ src/main/java/com/fot/atCurso/component/dates/OperationDatesImpl.java
@@ -0,0 +1,17 @@
+package com.fot.atCurso.component.dates;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OperationDatesImpl implements OperationDates {
+
+	@Override
+	public boolean compare(Date d1, Date d2) {
+		if(d1 == null && d2 == null) return true;
+		if(d1 == null) return false;
+		if(d2 == null) return false;
+		return d1.compareTo(d2) == 0;
+	}
+}
