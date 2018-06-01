81710a2f86406e295ba586473374bc9333651943
Orrequia <orrequia8@gmail.com>
2018-06-01 10:11:01 +0200

    Metodo para paginar todos los resultados de un usuario

--- src/main/java/com/fot/atCurso/dao/ResultDAO.java
+++ src/main/java/com/fot/atCurso/dao/ResultDAO.java
@@ -1,10 +1,17 @@
 package com.fot.atCurso.dao;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fot.atCurso.model.Result;
 
 @Repository
 public interface ResultDAO extends GenericDAO<Result>{
-		
+	
+	@Query(value = "SELECT FROM Result result WHERE result.idUser = :idUser ORDER BY result.date desc")
+	List<Result> findByUser(@Param("idUser") Integer idUser, Pageable p);
 }
