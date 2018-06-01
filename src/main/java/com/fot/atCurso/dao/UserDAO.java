5643f895ce1ffc1eb16c3416858dd091b3444917
Orrequia <orrequia8@gmail.com>
2018-06-01 13:53:20 +0200

    Añadida consulta para buscar los usuarios de un grupo paginado.

--- src/main/java/com/fot/atCurso/dao/UserDAO.java
+++ src/main/java/com/fot/atCurso/dao/UserDAO.java
@@ -1,9 +1,14 @@
 package com.fot.atCurso.dao;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.fot.atCurso.model.Result;
 import com.fot.atCurso.model.User;
 
 @Repository
@@ -12,4 +17,7 @@ public interface UserDAO extends GenericDAO<User> {
 	Optional<User> findOneByNameOrderByIdUserDesc(String name);	
 	
 	Optional<User> findOneByEmail(String email);
+	
+	@Query(value = "SELECT user from User user join Group group where group.idGroup = :idGroup")
+	List<User> findByCourse(@Param("idGroup") Integer idGroup, Pageable p);
 }
