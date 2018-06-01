d1d311bd3b0c8d962a3aa3f4c1d7700031bd84a0
Orrequia <orrequia8@gmail.com>
2018-06-01 11:39:33 +0200

    Creado los métodos, buscar un resultado del usuario, añadir un resultado al usuario, actualizar resultado de usuario, eliminar resultado del usuario y obtener un resultado lanzando excepción si no existe para ese usuario

--- src/main/java/com/fot/atCurso/service/result/ResultService.java
+++ src/main/java/com/fot/atCurso/service/result/ResultService.java
@@ -5,12 +5,19 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 
 import com.fot.atCurso.exceptions.NotFoundException;
+import com.fot.atCurso.exceptions.ObjectsDoNotMatchException;
 import com.fot.atCurso.model.Result;
+import com.fot.atCurso.model.User;
 import com.fot.atCurso.service.AbstractService;
 
 public interface ResultService  extends AbstractService<Result, Integer> {
 
 	List<Result> findResultByUser(Integer idUser, Pageable p) throws NotFoundException;
+	Result findOneResultByUser(Integer idUser, Integer idResult) throws NotFoundException;
+	Result addToUser(Result result, Integer idUser) throws NotFoundException;
+	void updateToUser(Integer idUser, Integer idResult, Result newResult) throws NotFoundException;
+	void deleteToUser(Integer idUser, Integer idResult, Result bodyResult) throws NotFoundException, ObjectsDoNotMatchException;
+	Result getAndCheck(User user, Integer idResult) throws NotFoundException;
 	
 	boolean isEqual(Result r1, Result r2);
 	void setValues(Result to, Result from);
--- src/main/java/com/fot/atCurso/service/result/ResultServiceImpl.java
+++ src/main/java/com/fot/atCurso/service/result/ResultServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.fot.atCurso.dao.ResultDAO;
 import com.fot.atCurso.dao.UserDAO;
 import com.fot.atCurso.exceptions.NotFoundException;
+import com.fot.atCurso.exceptions.ObjectsDoNotMatchException;
 import com.fot.atCurso.model.Result;
 import com.fot.atCurso.model.User;
 import com.fot.atCurso.service.AbstractServiceImpl;
@@ -43,5 +44,50 @@ public class ResultServiceImpl extends AbstractServiceImpl<Result, ResultDAO> im
 		final Optional<User> user = userService.findById(idUser);
 		user.orElseThrow(() -> new NotFoundException("El usuario no existe"));
 		return resultDAO.findByUser(idUser, PageRequest.of(p.getPageNumber(), p.getPageSize()));
+	}
+	
+	@Override
+	public Result findOneResultByUser(Integer idUser, Integer idResult) throws NotFoundException {
+		final Optional<User> user = userService.findById(idUser);
+		user.orElseThrow(() -> new NotFoundException("El usuario no existe"));
+		final Optional<Result> result = user.get().getResult().stream().filter(r -> r.getIdResult() == idResult).findFirst();
+		result.orElseThrow(() -> new NotFoundException("Este resultado no existe para este usuario"));
+		return result.get();
+	}
+	
+	@Override
+	public Result addToUser(Result result, Integer idUser) throws NotFoundException {
+		final Optional<User> user = userService.findById(idUser);
+		user.orElseThrow(() -> new NotFoundException("El usuario no existe"));
+		final Result createResult = create(result);
+		userService.addResult(user.get(), createResult);
+		return createResult;
+	}
+	
+	@Override
+	public void updateToUser(Integer idUser, Integer idResult, Result newResult) throws NotFoundException {
+		final Optional<User> user = userService.findById(idUser);
+		user.orElseThrow(() -> new NotFoundException("El usuario no existe"));
+		final Optional<Result> result = user.get().getResult().stream().filter(r -> r.getIdResult() == idResult).findFirst();
+		result.orElseThrow(() -> new NotFoundException("Este resultado no existe para este usuario"));
+		setValues(result.get(), newResult);
+		update(result.get());
+	}
+	
+	@Override
+	public void deleteToUser(Integer idUser, Integer idResult, Result bodyResult) throws NotFoundException, ObjectsDoNotMatchException {
+		final User user = userService.getAndCheck(idUser);
+		final Result result = getAndCheck(user, idResult);
+		if(!isEqual(bodyResult, result))
+			throw new ObjectsDoNotMatchException("El resultado recibido no coincide con el almacenado");
+		userService.removeResult(user, result);
+		delete(result);
+	}
+	
+	@Override
+	public Result getAndCheck(User user, Integer idResult) throws NotFoundException {
+		final Optional<Result> result = user.getResult().stream().filter(r -> r.getIdResult() == idResult).findFirst();
+		result.orElseThrow(() -> new NotFoundException("Este resultado no existe para este usuario"));
+		return result.get();
 	}
 }
