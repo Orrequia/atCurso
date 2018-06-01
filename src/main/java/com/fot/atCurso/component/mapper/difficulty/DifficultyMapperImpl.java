f708aebe801bf84b8524788cbf92aec7bd1a7973
Orrequia <orrequia8@gmail.com>
2018-06-01 09:22:47 +0200

    Añadida etiqueta Component

--- src/main/java/com/fot/atCurso/component/mapper/difficulty/DifficultyMapperImpl.java
+++ src/main/java/com/fot/atCurso/component/mapper/difficulty/DifficultyMapperImpl.java
@@ -1,9 +1,12 @@
 package com.fot.atCurso.component.mapper.difficulty;
 
+import org.springframework.stereotype.Component;
+
 import com.fot.atCurso.component.mapper.AbstractMapper;
 import com.fot.atCurso.dto.difficulty.DifficultyDTO;
 import com.fot.atCurso.model.Difficulty;
 
+@Component
 public class DifficultyMapperImpl extends AbstractMapper<Difficulty, DifficultyDTO> implements DifficultyMapper {
 	
 	@Override
