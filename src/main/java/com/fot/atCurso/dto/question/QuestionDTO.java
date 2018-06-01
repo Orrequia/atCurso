dc1d101716a28a36eb3a851a401397cf6f5e4154
Orrequia <orrequia8@gmail.com>
2018-06-01 15:11:44 +0200

    DTO de la pregunta con su lista de respuestas

---
+++ src/main/java/com/fot/atCurso/dto/question/QuestionDTO.java
@@ -0,0 +1,17 @@
+package com.fot.atCurso.dto.question;
+
+import java.util.List;
+
+import com.fot.atCurso.dto.answer.AnswerDTO;
+
+import lombok.Data;
+
+@Data
+public class QuestionDTO {
+
+	private Integer idQuestion;
+	private String name;
+	private Integer tag;
+	private Integer difficulty;
+	private List<AnswerDTO> anwers;
+}
