824fb098ceab1afb74ba8f5ee92e10f054c830aa
Orrequia <orrequia8@gmail.com>
2018-06-01 09:21:29 +0200

    Añadida etiqueta Component

--- src/main/java/com/fot/atCurso/component/mapper/tag/TagMapperImpl.java
+++ src/main/java/com/fot/atCurso/component/mapper/tag/TagMapperImpl.java
@@ -1,9 +1,12 @@
 package com.fot.atCurso.component.mapper.tag;
 
+import org.springframework.stereotype.Component;
+
 import com.fot.atCurso.component.mapper.AbstractMapper;
 import com.fot.atCurso.dto.tag.TagDTO;
 import com.fot.atCurso.model.Tag;
 
+@Component
 public class TagMapperImpl extends AbstractMapper<Tag, TagDTO> implements TagMapper {
 	
 	@Override
