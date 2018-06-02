f157e2f5c2f0865933713f7f47c768f5a6fe64a6
Orrequia <orrequia8@gmail.com>
2018-06-02 21:13:22 +0200

    Creado método para buscar por un usuario

--- src/main/java/com/fot/atCurso/dao/CourseDAO.java
+++ src/main/java/com/fot/atCurso/dao/CourseDAO.java
@@ -1,11 +1,16 @@
 package com.fot.atCurso.dao;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import com.fot.atCurso.model.Course;
+import com.fot.atCurso.model.User;
 
 @Repository
 public interface CourseDAO extends GenericDAO<Course> {
 
+	List<Course> findByUser(User user, Pageable p);
 }
 
