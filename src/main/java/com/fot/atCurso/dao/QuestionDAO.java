2e375a1f36ab8e3c13dba2ff3d50f502bbaa20d0
Orrequia <orrequia8@gmail.com>
2018-06-02 21:03:56 +0200

    Añadido método para obtener preguntas de un tag concreto

--- src/main/java/com/fot/atCurso/dao/QuestionDAO.java
+++ src/main/java/com/fot/atCurso/dao/QuestionDAO.java
@@ -1,10 +1,15 @@
 package com.fot.atCurso.dao;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import com.fot.atCurso.model.Question;
+import com.fot.atCurso.model.Tag;
 
 @Repository
 public interface QuestionDAO extends GenericDAO<Question>{
 
+	List<Question> findByTag(Tag tag, Pageable p);
 }
