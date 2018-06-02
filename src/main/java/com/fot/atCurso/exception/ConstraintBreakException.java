dffe151b0878cfc1ecbe346d222b38e598b9091a
Orrequia <orrequia8@gmail.com>
2018-06-02 08:09:07 +0200

    Cambiado mensaje por defecto de la excepción

--- src/main/java/com/fot/atCurso/exception/ConstraintBreakException.java
+++ src/main/java/com/fot/atCurso/exception/ConstraintBreakException.java
@@ -3,7 +3,7 @@ package com.fot.atCurso.exception;
 public class ConstraintBreakException extends Exception {
 	private static final long serialVersionUID = 898967L;
 
-	public static final String MSG = "Esta entiedad no existe";
+	public static final String MSG = "Hay restricciones no cumplidas.";
 	
 	public ConstraintBreakException(String message) {
 		super(message);
