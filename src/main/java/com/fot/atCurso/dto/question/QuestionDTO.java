f422410d0f5c13d5694aed271ca8f589608490d1
Orrequia <orrequia8@gmail.com>
2018-06-02 10:16:13 +0200

    Cambiado nombres de los tag y difficulty por idTag e idDifficulty, para que Dozer no trabaje con ellos

--- src/main/java/com/fot/atCurso/dto/question/QuestionDTO.java
+++ src/main/java/com/fot/atCurso/dto/question/QuestionDTO.java
@@ -11,7 +11,7 @@ public class QuestionDTO {
 
 	private Integer idQuestion;
 	private String name;
-	private Integer tag;
-	private Integer difficulty;
+	private Integer idTag;
+	private Integer idDifficulty;
 	private List<AnswerDTO> answers;
 }
