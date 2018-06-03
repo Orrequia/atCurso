372d01ff7923b224bf293302a9eec3f55ad599c5
Orrequia <orrequia8@gmail.com>
2018-06-03 21:10:24 +0200

    Añadida excepciones al método de responder una pregunta.

--- src/main/java/com/fot/atCurso/service/selection/SelectionService.java
+++ src/main/java/com/fot/atCurso/service/selection/SelectionService.java
@@ -2,6 +2,9 @@ package com.fot.atCurso.service.selection;
 
 import java.util.List;
 
+import com.fot.atCurso.exception.AlreadyDoneException;
+import com.fot.atCurso.exception.ExceededTimeException;
+import com.fot.atCurso.exception.NotFoundException;
 import com.fot.atCurso.model.Answer;
 import com.fot.atCurso.model.Question;
 import com.fot.atCurso.model.Quiz;
@@ -17,6 +20,6 @@ public interface SelectionService extends AbstractService<Selection, Integer> {
 	Selection create(User user, Quiz quiz, Question question);
 	List<Selection> create(User user, Quiz quiz, List<Question> questions);
 	
-	void answerTheQuestion(User user, Quiz quiz, Question question, Answer answer);
+	void answerTheQuestion(User user, Quiz quiz, Question question, Answer answer) throws ExceededTimeException, AlreadyDoneException, NotFoundException;
 	boolean allQuestionsBeenAnswered(User user, Quiz quiz);
 }
