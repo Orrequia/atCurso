fbfbaf78347b572d72c4143d7deb1f30699e49e4
Orrequia <orrequia8@gmail.com>
2018-06-03 21:07:57 +0200

    Añadido método para obtener la respuesta correcta de la pregunta.

--- src/main/java/com/fot/atCurso/service/answer/AnswerService.java
+++ src/main/java/com/fot/atCurso/service/answer/AnswerService.java
@@ -1,10 +1,15 @@
 package com.fot.atCurso.service.answer;
 
+import com.fot.atCurso.exception.AlreadyDoneException;
+import com.fot.atCurso.exception.ExceededTimeException;
 import com.fot.atCurso.exception.NotFoundException;
 import com.fot.atCurso.model.Answer;
+import com.fot.atCurso.model.Question;
 import com.fot.atCurso.service.AbstractService;
 
 public interface AnswerService extends AbstractService<Answer, Integer> {
 
-	Answer addAnswerToSelection(Integer idUser, Integer idQuiz, Integer idQuestion, Answer answer) throws NotFoundException;
+	Answer addAnswerToSelection(Integer idUser, Integer idQuiz, Integer idQuestion, Answer answer) throws NotFoundException, ExceededTimeException, AlreadyDoneException;
+	
+	Answer getCorrect(Question question);
 }
