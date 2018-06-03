4bcb4880c126245056bc34017ac8be55adf8bdca
Orrequia <orrequia8@gmail.com>
2018-06-03 09:44:57 +0200

    Creado dos métodos para comprobar si es la primera vez que el usuario accede a este cuestionario y para crear una lista de selections

--- src/main/java/com/fot/atCurso/service/selection/SelectionService.java
+++ src/main/java/com/fot/atCurso/service/selection/SelectionService.java
@@ -1,8 +1,16 @@
 package com.fot.atCurso.service.selection;
 
+import java.util.List;
+
+import com.fot.atCurso.model.Question;
+import com.fot.atCurso.model.Quiz;
 import com.fot.atCurso.model.Selection;
+import com.fot.atCurso.model.User;
 import com.fot.atCurso.service.AbstractService;
 
 public interface SelectionService extends AbstractService<Selection, Integer> {
 
+	boolean isFirstTime(User user, Quiz quiz);
+	
+	List<Selection> create(User user, Quiz quiz, List<Question> questions);
 }
--- src/main/java/com/fot/atCurso/service/selection/SelectionServiceImpl.java
+++ src/main/java/com/fot/atCurso/service/selection/SelectionServiceImpl.java
@@ -1,13 +1,40 @@
 package com.fot.atCurso.service.selection;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.fot.atCurso.dao.SelectionDAO;
+import com.fot.atCurso.model.Question;
+import com.fot.atCurso.model.Quiz;
 import com.fot.atCurso.model.Selection;
+import com.fot.atCurso.model.User;
 import com.fot.atCurso.service.AbstractServiceImpl;
 
 public class SelectionServiceImpl extends AbstractServiceImpl<Selection, SelectionDAO> implements SelectionService {
 	
 	@Autowired
 	SelectionDAO selectionDAO;
+	
+	@Override
+	public boolean isFirstTime(User user, Quiz quiz) {
+		return selectionDAO.findByUserAndQuiz(user, quiz).size() == 0;
+	}
+	
+	@Override
+	public List<Selection> create(User user, Quiz quiz, List<Question> questions) {
+		Date askedDate = new Date();
+		List<Selection> selections = new ArrayList<Selection>();
+		for(Question q : questions) {
+			Selection selection = new Selection();
+			selection.setUser(user);
+			selection.setQuiz(quiz);
+			selection.setQuestion(q.getName());
+			selection.setAskedDate(askedDate);
+			selections.add(selection);
+		}
+		return selections;
+	}
 }
