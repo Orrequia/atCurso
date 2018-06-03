12a8ee846daa8c7b281d356807d11a0a0d3f194f
Orrequia <orrequia8@gmail.com>
2018-06-03 11:05:36 +0200

    Añadida excepciones controladas

--- src/main/java/com/fot/atCurso/controller/UserQuizQuestionController.java
+++ src/main/java/com/fot/atCurso/controller/UserQuizQuestionController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fot.atCurso.component.mapper.question.QuestionMapper;
+import com.fot.atCurso.exception.CannotGetNewQuestionWithAnswerBeforeException;
+import com.fot.atCurso.exception.CompletedQuizException;
 import com.fot.atCurso.exception.NotFoundException;
 import com.fot.atCurso.exception.ParametersNotAllowedException;
 import com.fot.atCurso.model.Question;
@@ -28,7 +30,7 @@ public class UserQuizQuestionController {
 	@GetMapping
 	public List<Question> getQuestions(@RequestParam(defaultValue="0", required=false) Integer page, 
 			 	@RequestParam(defaultValue="10", required=false) Integer size,
-			 	@PathVariable("idUser") Integer idUser, @PathVariable("idQuiz") Integer idQuiz) throws ParametersNotAllowedException, NotFoundException {
+			 	@PathVariable("idUser") Integer idUser, @PathVariable("idQuiz") Integer idQuiz) throws ParametersNotAllowedException, NotFoundException, CannotGetNewQuestionWithAnswerBeforeException, CompletedQuizException {
 		return questionService.getAndCheckQuestions(idUser, idQuiz);
 	}
 }
