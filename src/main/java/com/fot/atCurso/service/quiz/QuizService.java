bf0c331b915655a96d69ccfc78a4c0ade5fdb5d7
Orrequia <orrequia8@gmail.com>
2018-06-03 22:58:55 +0200

    Añadida posibles excepciones

--- src/main/java/com/fot/atCurso/service/quiz/QuizService.java
+++ src/main/java/com/fot/atCurso/service/quiz/QuizService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 
+import com.fot.atCurso.exception.ConstraintBreakException;
 import com.fot.atCurso.exception.NotFoundException;
 import com.fot.atCurso.exception.UnequalObjectsException;
 import com.fot.atCurso.model.Course;
@@ -14,8 +15,8 @@ public interface QuizService  extends AbstractService<Quiz, Integer> {
 	
 	List<Quiz> findQuizByCourse(Integer idCourse, Pageable p) throws NotFoundException;
 	Quiz findOneQuizByCourse(Integer idCourse, Integer idQuiz) throws NotFoundException;
-	Quiz addToCourse(Integer idCourse, Quiz quiz) throws NotFoundException;
-	void updateToCourse(Integer idCourse, Integer idQuiz, Quiz newQuiz) throws NotFoundException;
+	Quiz addToCourse(Integer idCourse, Quiz quiz) throws NotFoundException, ConstraintBreakException;
+	void updateToCourse(Integer idCourse, Integer idQuiz, Quiz newQuiz) throws NotFoundException, ConstraintBreakException;
 	void deleteToCourse(Integer idCourse, Integer idQuiz, Quiz bodyQuiz) throws NotFoundException, UnequalObjectsException;
 	
 	Quiz getAndCheck(Integer idQuiz) throws NotFoundException;
