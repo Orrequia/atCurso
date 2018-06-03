75895b3fb82dcdc749add4fb264cf78fc85c59a7
Orrequia <orrequia8@gmail.com>
2018-06-03 13:30:26 +0200

    Añadido método para añadir la respuesta a la selección del usuario.

--- src/main/java/com/fot/atCurso/service/answer/AnswerService.java
+++ src/main/java/com/fot/atCurso/service/answer/AnswerService.java
@@ -1,8 +1,10 @@
 package com.fot.atCurso.service.answer;
 
+import com.fot.atCurso.exception.NotFoundException;
 import com.fot.atCurso.model.Answer;
 import com.fot.atCurso.service.AbstractService;
 
 public interface AnswerService extends AbstractService<Answer, Integer> {
 
+	Answer addAnswerToSelection(Integer idUser, Integer idQuiz, Integer idQuestion, Answer answer) throws NotFoundException;
 }
