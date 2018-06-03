94f3b122b60db895f036360db271a8f41250d81f
Orrequia <orrequia8@gmail.com>
2018-06-03 21:07:24 +0200

    Excepción para informar de que el tiempo se ha excedido.

---
+++ src/main/java/com/fot/atCurso/exception/ExceededTimeException.java
@@ -0,0 +1,15 @@
+package com.fot.atCurso.exception;
+
+public class ExceededTimeException extends Exception {
+	private static final long serialVersionUID = 856465676675L;
+
+	public static final String MSG = "Se ha excedido el tiempo limitado.";
+	
+	public ExceededTimeException(String message) {
+		super(message);
+	}
+	
+	public ExceededTimeException() {
+		super(MSG);
+	}
+}
