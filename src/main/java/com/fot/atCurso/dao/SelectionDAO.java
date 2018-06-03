7288bfe133a48d1ef94c3185223216d96d491d97
Orrequia <orrequia8@gmail.com>
2018-06-03 09:43:52 +0200

    Añadido consulta para obtener los selection de un usuario basado en un cuestionario

--- src/main/java/com/fot/atCurso/dao/SelectionDAO.java
+++ src/main/java/com/fot/atCurso/dao/SelectionDAO.java
@@ -1,10 +1,15 @@
 package com.fot.atCurso.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
+import com.fot.atCurso.model.Quiz;
 import com.fot.atCurso.model.Selection;
+import com.fot.atCurso.model.User;
 
 @Repository
 public interface SelectionDAO extends GenericDAO<Selection>{
 
+	List<Selection> findByUserAndQuiz(User user, Quiz quiz);
 }
