4b662918ec44ee798a362ba1dd0e8f0dd5fb3692
Orrequia <orrequia8@gmail.com>
2018-06-03 11:15:01 +0200

    Cambiado tipo de return a lista de QuestionDTO

--- src/main/java/com/fot/atCurso/controller/UserQuizQuestionController.java
+++ src/main/java/com/fot/atCurso/controller/UserQuizQuestionController.java
@@ -6,15 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fot.atCurso.component.mapper.question.QuestionMapper;
+import com.fot.atCurso.dto.question.QuestionDTO;
 import com.fot.atCurso.exception.CannotGetNewQuestionWithAnswerBeforeException;
 import com.fot.atCurso.exception.CompletedQuizException;
 import com.fot.atCurso.exception.NotFoundException;
 import com.fot.atCurso.exception.ParametersNotAllowedException;
-import com.fot.atCurso.model.Question;
 import com.fot.atCurso.service.question.QuestionService;
 
 @RestController
@@ -28,9 +27,8 @@ public class UserQuizQuestionController {
 	QuestionMapper questionMapper;
 	
 	@GetMapping
-	public List<Question> getQuestions(@RequestParam(defaultValue="0", required=false) Integer page, 
-			 	@RequestParam(defaultValue="10", required=false) Integer size,
-			 	@PathVariable("idUser") Integer idUser, @PathVariable("idQuiz") Integer idQuiz) throws ParametersNotAllowedException, NotFoundException, CannotGetNewQuestionWithAnswerBeforeException, CompletedQuizException {
-		return questionService.getAndCheckQuestions(idUser, idQuiz);
+	public List<QuestionDTO> getQuestions(@PathVariable("idUser") Integer idUser, 
+			@PathVariable("idQuiz") Integer idQuiz) throws ParametersNotAllowedException, NotFoundException, CannotGetNewQuestionWithAnswerBeforeException, CompletedQuizException {
+		return questionMapper.modelToDto(questionService.getAndCheckQuestions(idUser, idQuiz));
 	}
 }
